9885b668c7bd959abfd7bbee0e2c3e6d7b8dd9c7
Oleh <oleh@localhost.localdomain>
2017-08-04 10:20:07 +0300

    add display comments for post

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
     protected $fillable = array('title', 'body');
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,21 @@
 
         @include('posts.post', compact('post'))
 
+        <div class="comments">
+
+            @foreach($post->comments as $comment)
+
+                <blockquote>
+                    <p>{{$comment->body}}</p>
+                    <footer>
+                        {{$comment->created_at->diffForHumans()}} by <cite title="Source Title">ZloyLeva</cite>
+                    </footer>
+                </blockquote>
+
+            @endforeach
+
+        </div>
+
     </div><!-- /.blog-main -->
 
 @stop
